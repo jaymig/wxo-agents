01aca7f2f1fba6ef1274647bf0cd1b13830473ec
Jay Migliaccio <jaymig@gmail.com>
2025-11-25 15:39:30 -0800

    Update remove.sh

--- context/remove.sh
+++ context/remove.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -x
 
-# remove agents
+
+## Un deploy agents
+orchestrate agents undeploy --name context_collaborator_agent
+orchestrate agents undeploy --name context_supervisor_agent
+
+## remove agents
 orchestrate agents remove -n context_supervisor_agent -k native
 orchestrate agents remove -n context_collaborator_agent -k native
 
-# remove tool
+## remove tool
 orchestrate tools remove --name return_context
 
 
