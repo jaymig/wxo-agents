3633fb331cce57478111c947e04ce66d787d346f
Jay Migliaccio <jaymig@gmail.com>
2025-11-25 15:38:41 -0800

    Update deploy.sh

--- context/deploy.sh
+++ context/deploy.sh
@@ -10,3 +10,7 @@ orchestrate agents import -f ./agents/context_collaborator_agent.json
 ## Import supervisor agent
 orchestrate agents import -f ./agents/context_supervisor_agent.json
 
+## deploy agents
+
+orchestrate agents deploy --name context_collaborator_agent
+orchestrate agents deploy --name context_supervisor_agent
