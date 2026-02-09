70a9cecc7916462a77e996da99edee67d16f4bef
Jay Migliaccio <jaymig@gmail.com>
2026-02-08 22:01:08 -0800

    Update deploy.sh

--- user_context/deploy.sh
+++ user_context/deploy.sh
@@ -3,6 +3,7 @@ set -x
 
 ## import tools
 orchestrate tools import -k python -f ./tools/user_context.py -r ./tools/requirements.txt
+orchestrate tools import -k python -f ./tools/set_context.py -r ./tools/requirements.txt
 
 ## Import Welcome agent
 orchestrate agents import -f ./agents/welcome_agent.yaml
