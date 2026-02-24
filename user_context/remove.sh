e0bdca9a61d6ba735c88a86105d0ce233cc82293
Jay Migliaccio <jaymig@gmail.com>
2026-02-23 21:57:07 -0800

    Update remove.sh

--- user_context/remove.sh
+++ user_context/remove.sh
@@ -7,3 +7,6 @@ orchestrate agents remove -n welcome_agent -k native
 # remove tool
 orchestrate tools remove --name session_context
 orchestrate tools remove --name user_info
+orchestrate tools remove --name update_user_context
+orchestrate tools remove --name update_user_profile
+orchestrate tools remove --name read_user_profile
