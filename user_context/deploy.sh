25fa9025ec8f4cdd77aa1fb7f0e501e15d309dd7
Jay Migliaccio <jaymig@gmail.com>
2026-02-24 16:07:14 -0800

    Update deploy.sh

--- user_context/deploy.sh
+++ user_context/deploy.sh
@@ -2,15 +2,15 @@
 set -x
 
 ## import tools
-orchestrate tools import -k python -f ./tools/user_context.py -r ./tools/requirements.txt
-orchestrate tools import -k python -f ./tools/set_context.py -r ./tools/requirements.txt
-orchestrate tools import -k python -f ./tools/set_user_profile.py -r ./tools/requirements.txt
+orchestrate tools import -k python -f ./tools/user_info.py -r ./tools/requirements.txt
+orchestrate tools import -k python -f ./tools/update_user_context.py -r ./tools/requirements.txt
+orchestrate tools import -k python -f ./tools/session_context.py -r ./tools/requirements.txt
+orchestrate tools import -k python -f ./tools/update_user_profile.py -r ./tools/requirements.txt
+orchestrate tools import -k python -f ./tools/read_user_profile.py -r ./tools/requirements.txt
 
 ## Import Welcome agent
 orchestrate agents import -f ./agents/welcome_agent.yaml
 
 ## Publish agent to live environment
 orchestrate agents deploy --name welcome_agent
-
-
 
